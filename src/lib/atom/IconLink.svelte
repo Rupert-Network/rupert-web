f52bb021f0d8917b942ec2e77eb943fd1da39f4a
DeusFerrariis <cleghornw@protonmail.com>
2022-01-28 22:34:04 -0700

    Separated social media links into IconLinks

---
+++ src/lib/atom/IconLink.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Fa from "svelte-fa/src/fa.svelte";
+  import { faGithub, faTwitter, faInstagram } from "@fortawesome/free-brands-svg-icons";
+  export let link = "#"
+  export let size = "lg"
+
+  let icon = faGithub;
+  if (link.indexOf("instagram") >= 0) {
+    icon = faInstagram;
+  }
+  else if (link.indexOf("twitter") >= 0) {
+    icon = faTwitter;
+  }
+
+</script>
+
+<a href={link} target="_blank">
+  <Fa style = "color: white;" {icon} size="lg"/>
+</a>
